5a92d9cf5f0f47ae1a5bc2fdc1a07013ddfe60ab
tkki <1411remember@naver.com>
2021-08-07 23:36:47 +0900

    feat: 그리디 2. 숫자 카드 게임

--- 1. 그리디/2. 숫자 카드 게임.py
+++ 1. 그리디/2. 숫자 카드 게임.py
@@ -4,3 +4,17 @@
             둘째 줄부터 N개의 줄에 걸쳐 각 카드에 적힌 숫자가 주어진다. 각 숫자는 1 이상 10000 이하의 자연수이다.
     출력조건: 첫째 줄에 게임의 룰에 맞게 선택한 카드에 적힌 숫자를 출력한다.
 """
+N, M = map(int, input().split())
+
+matrix = []
+for i in range(N):
+  temp = list(map(int, input().split()))
+  assert len(temp) == M, print("should be same length")
+  matrix.append(temp)
+
+_res = 0
+for i in range(N):
+  temp_min = min(matrix[i])
+  _res = max(_res, temp_min)
+
+print(_res)
